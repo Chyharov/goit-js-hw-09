cd1f2c01b0f0780936f02b566a97b784faeef507
Oleksii Chyharov <chyharov@gmail.com>
2022-07-10 22:22:31 +0300

    color-switcher - rdy

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStartEl = document.querySelector("[data-start]");
+const btnStopEl = document.querySelector("[data-stop]");
+let changeColor = null;
 
+btnStopEl.setAttribute('disabled', '');
+
+btnStartEl.addEventListener('click', onButtonStartClick);
+btnStopEl.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClick() {
+    changeColor = setInterval(changeBodyBackgroundColor, 1000);
+    btnStartEl.disabled = true;
+    btnStopEl.disabled = false;
+}
+
+function onButtonStopClick() {
+    clearInterval(changeColor);
+    btnStartEl.disabled = false;
+    btnStopEl.disabled = true;
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  function changeBodyBackgroundColor() {
+      document.body.style.backgroundColor = getRandomHexColor();
+  }
