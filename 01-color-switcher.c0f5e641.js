d7010dba8608e75b07df32284f8b6abb6b25a191
Chyharov <102535915+Chyharov@users.noreply.github.com>
2022-07-10 19:22:48 +0000

    Deploying to gh-pages from @ Chyharov/goit-js-hw-09@cd1f2c01b0f0780936f02b566a97b784faeef507 🚀

---
+++ 01-color-switcher.c0f5e641.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;function n(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.setAttribute("disabled",""),t.addEventListener("click",(function(){d=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.c0f5e641.js.map
